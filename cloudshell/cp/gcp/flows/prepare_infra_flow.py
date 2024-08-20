6c16c8df8e85a50a4606f5dfe4f3251690b78208
alexquali <alexey.b@quali.com>
2024-08-20 19:49:41 +0200

    Added default service account credentials determination

--- cloudshell/cp/gcp/flows/prepare_infra_flow.py
+++ cloudshell/cp/gcp/flows/prepare_infra_flow.py
@@ -1,58 +1,67 @@
-from logging import Logger
+from __future__ import annotations
+
+from attrs import define, field
+from typing import TYPE_CHECKING
 
 from cloudshell.cp.core.flows.prepare_sandbox_infra import (
     AbstractPrepareSandboxInfraFlow,
 )
-from cloudshell.cp.core.request_actions import (
-    PrepareSandboxInfraRequestActions as actions,
-)
-from cloudshell.cp.core.reservation_info import ReservationInfo
-from cloudshell.cp.core.utils import generate_ssh_key_pair
 
+from cloudshell.cp.core.utils import generate_ssh_key_pair
 from cloudshell.cp.gcp.handlers.ssh_keys import SSHKeysHandler
 from cloudshell.cp.gcp.handlers.subnet import SubnetHandler
 from cloudshell.cp.gcp.handlers.vpc import VPCHandler
 from cloudshell.cp.gcp.helpers.name_generator import generate_name, generate_vpc_name
-from cloudshell.cp.gcp.resource_conf import GCPResourceConfig
 
 
+if TYPE_CHECKING:
+    from logging import Logger
+    from cloudshell.cp.core.reservation_info import ReservationInfo
+    from cloudshell.cp.core.request_actions import (
+        PrepareSandboxInfraRequestActions as RequestActions,
+    )
+    from cloudshell.cp.gcp.resource_conf import GCPResourceConfig
+
+
+@define
 class PrepareGCPInfraFlow(AbstractPrepareSandboxInfraFlow):
-    def __init__(
-        self,
-        logger: Logger,
-        resource_config: GCPResourceConfig,
-        reservation_info: ReservationInfo,
-    ):
-        super().__init__(logger)
-        self.config = resource_config
-        self.reservation_info = reservation_info
-        self._vpc = None
+    logger: Logger
+    config: GCPResourceConfig
+    reservation_info: ReservationInfo
+    vpc: str = field(init=False, default=None)
 
-    def prepare_common_objects(self, request_actions: actions):
+    def __attrs_pre_init__(self):
+        super().__init__(self.logger)
+
+    def prepare_common_objects(self, request_actions: RequestActions) -> None:
+        """"""
         vpc_handler = VPCHandler(self.config.credentials)
-        self._vpc = vpc_handler.get_or_create_vpc(
+        self.vpc = vpc_handler.get_or_create_vpc(
             generate_vpc_name(self.reservation_info.reservation_id)
         )
 
-    def prepare_cloud_infra(self, request_actions: actions):
-        # create vpc using vpc handler
-        return self._vpc
+    def prepare_cloud_infra(self, request_actions: RequestActions) -> str:
+        """Create vpc using vpc handler."""
+        return self.vpc
 
-    def prepare_subnets(self, request_actions: actions):
-        # create subnets using subnet handler
+    def prepare_subnets(self, request_actions: RequestActions) -> dict[str, str]:
+        """Create subnets using subnet handler."""
         subnet_results = {}
         subnet_handler = SubnetHandler(self.config.credentials, self.config.region)
         for subnet_request in request_actions.prepare_subnets:
-            subnet_results[
-                subnet_request.actionId
-            ] = subnet_handler.get_or_create_subnet(
-                network_name=self._vpc,
-                ip_cidr_range=subnet_request.get_cidr(),
-                subnet_name=generate_name(subnet_request.get_alias()),
+            subnet_results.update(
+                {
+                    subnet_request.actionId: subnet_handler.get_or_create_subnet(
+                        network_name=self.vpc,
+                        ip_cidr_range=subnet_request.get_cidr(),
+                        subnet_name=generate_name(subnet_request.get_alias())
+                    )
+                }
             )
         return subnet_results
 
-    def create_ssh_keys(self, request_actions: actions):
+    def create_ssh_keys(self, request_actions: RequestActions) -> str:
+        """Create SSH Keys."""
         ssh_keys_handler = SSHKeysHandler(self.config.credentials)
         private_key, public_key = ssh_keys_handler.get_ssh_key_pair(
             bucket_name=self.config.keypairs_location,
--- cloudshell/cp/gcp/helpers/converters.py
+++ cloudshell/cp/gcp/helpers/converters.py
@@ -5,6 +5,7 @@ from contextlib import suppress
 from typing import TYPE_CHECKING
 
 from google.oauth2 import service_account
+from google.auth import default, exceptions
 
 from cloudshell.cp.gcp.helpers.errors import AttributeGCPError
 
@@ -13,14 +14,19 @@ if TYPE_CHECKING:
 
 
 def get_credentials(account_info: str) -> Credentials:
-    """"""
-    # assume that json data provided
-    with suppress(AttributeError):
-        account_dict = json.loads(account_info)
-        return service_account.Credentials.from_service_account_info(account_dict)
+    """Get Account Credentials."""
+    if not account_info:
+        # Try to get default credentials. Can be used when CS deployed in GCP
+        with suppress(exceptions.DefaultCredentialsError):
+            return default()
+    else:
+        # Assume that json data provided
+        with suppress(AttributeError):
+            account_dict = json.loads(account_info)
+            return service_account.Credentials.from_service_account_info(account_dict)
 
-    # otherwise, the path to the configuration file is expected
-    with suppress(FileNotFoundError):
-        return service_account.Credentials.from_service_account_file(account_info)
+        # Otherwise, the path to the configuration file is expected
+        with suppress(FileNotFoundError):
+            return service_account.Credentials.from_service_account_file(account_info)
 
     raise AttributeGCPError("Cannot get service account information.")
